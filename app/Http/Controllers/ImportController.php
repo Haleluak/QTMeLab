ca5a50cb94082e49c98df3b9d6c8fbea0ba520c3
HuuDuc <haleluak@gmail.com>
2017-12-14 17:41:37 +0700

    insert contract

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -2,29 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Repository\ContractRepository;
+use App\Repository\CustomerRepository;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Input;
-use Maatwebsite\Excel\Excel;
 
 class ImportController extends Controller
 {
+    private $_contractRepository;
+
+    /**
+     * ShopifyController constructor.
+     */
+    public function __construct()
+    {
+        $this->_contractRepository = new ContractRepository();
+        $this->_customerRepository = new CustomerRepository();
+    }
+
     public function index()
     {
         return view('import.index');
     }
+
     public function importContract(Request $request)
     {
-        if($request->hasFile('imported-file')){
+        if ($request->hasFile('imported-file')) {
             $path = $request->file('imported-file')->getRealPath();
             $data = \Excel::load($path)->get();
 
-            if($data->count()){
+            if ($data->count()) {
                 foreach ($data as $key => $value) {
-                    //$arr[] = ['title' => $value->title, 'body' => $value->body];
-                    $arr[] = $data[$key];
-                }
-                if(!empty($arr)){
-                    dd($arr);
+                    $arr = [
+                        'so_hop_dong' => $value->so_hop_dong,
+                        'ngay_lap_hd' => $value->ngay_lap_hd,
+                        'ngay_du_kien' => $value->ngay_du_kien
+                    ];
+                    $id_contract =  $this->_contractRepository->insert($arr);
+
+                    $cus = [
+                        'contract_id' => $id_contract,
+                        'ten_khach_hang_thanh_toan' => $value->ten_khach_hang_thanh_toan,
+                        'dia_chi_khach_hang_thanh_toan' => $value->dia_chi_khach_hang_thanh_toan,
+                        'so_dien_thoai' => $value->so_dien_thoai,
+                        'ma_so_thue' => $value->ma_so_thue,
+                        'khach_hang_ket_qua' => $value->khach_hang_ket_qua,
+                        'dia_chi_khach_hang_ket_qua' => $value->dia_chi_khach_hang_ket_qua,
+                    ];
+                    $this->_customerRepository->insert($cus);
                 }
             }
         }
